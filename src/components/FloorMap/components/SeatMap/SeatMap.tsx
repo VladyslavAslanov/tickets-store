822de5f4cf80c70db235486c1f79e255c350d24e
VladyslavAslanov <vlad.aslanov@icloud.com>
2023-10-30 12:58:49 +0100

    Feat: new mock models

--- src/components/FloorMap/components/SeatMap/SeatMap.tsx
+++ src/components/FloorMap/components/SeatMap/SeatMap.tsx
@@ -3,7 +3,8 @@ import seatModel from '../../../../models/seats_model.json'
 import priceModel from '../../../../models/price_model.json'
 import classes from './SeatMap.module.sass'
 import PriceList from "../../../Prices/PriceList/PriceList";
-import SeatItem from "../SeatItem/SeatItem";
+import {Layer, Stage} from "react-konva";
+import {Circle} from "react-konva/lib/ReactKonvaCore";
 
 interface SeatMapProps {
     priceList: number[]
@@ -28,8 +29,7 @@ const SeatMap: FC<SeatMapProps> = ({
                                        },
                                        cart = []
                                    }) => {
-    const colors: colorList[] = [
-        {
+    const colors: colorList[] = [{
         category: "Category 1",
         color: "#FF6B9B"
     }, {
@@ -61,39 +61,94 @@ const SeatMap: FC<SeatMapProps> = ({
                 <div className={classes.stage}>
                     Stage
                 </div>
-                <div className={classes.seats}>
-                    {seats.map(({
-                                    capacity,
-                                    capacityLeft,
-                                    eventPriceId,
-                                    eventSeatId,
-                                    place,
-                                    rowNum,
-                                    type,
-                                    x,
-                                    y
-                                }, seatIndex) => {
-                        const seatPriceModel = prices.find(el => el.id === eventPriceId);
+                <Stage width={500} height={500}>
+                    <Layer>
+                        {seats.map(({
+                                        capacity,
+                                        capacityLeft,
+                                        eventPriceId,
+                                        eventSeatId,
+                                        place,
+                                        rowNum,
+                                        type,
+                                        x,
+                                        y
+                                    }, seatIndex) => {
+                            const seatPriceModel = prices.find(el => el.id === eventPriceId);
 
-                        return (
-                            <SeatItem key={seatIndex}
-                                      capacity={capacity}
-                                      capacityLeft={capacityLeft}
-                                      eventPriceId={eventPriceId}
-                                      eventSeatId={eventSeatId}
-                                      place={place}
-                                      rowNum={rowNum}
-                                      type={type}
-                                      x={x}
-                                      y={y}
-                                      category={seatPriceModel === undefined ? "" : seatPriceModel.name}
-                                      price={seatPriceModel === undefined ? 0 : seatPriceModel.price}
-                                      currency={seatPriceModel === undefined ? "" : seatPriceModel.currency}
-                                      id={seatPriceModel === undefined ? 0 : seatPriceModel.id}
-                                      cart={cart}
-                                      onTicketAdd={onTicketAdd}/>
-                        );
-                    })}
+                            return (
+                                <Circle
+                                    capacity={capacity}
+                                    capacityLeft={capacityLeft}
+                                    eventPriceId={eventPriceId}
+                                    eventSeatId={eventSeatId}
+                                    place={place}
+                                    rowNum={rowNum}
+                                    type={type}
+                                    x={x}
+                                    y={y}
+                                    draggable={true}
+                                    radius={5}
+                                    fill="#FF00FF"
+                                    category={seatPriceModel === undefined ? "" : seatPriceModel.name}
+                                    price={seatPriceModel === undefined ? 0 : seatPriceModel.price}
+                                    currency={seatPriceModel === undefined ? "" : seatPriceModel.currency}
+                                    id={seatPriceModel === undefined ? "" : seatPriceModel.id.toString()}
+                                    cart={cart}
+                                    onClick={(e) => onTicketAdd(123, "Cat 1", 3)}/>
+                                // <SeatItem key={seatIndex}
+                                //           capacity={capacity}
+                                //           capacityLeft={capacityLeft}
+                                //           eventPriceId={eventPriceId}
+                                //           eventSeatId={eventSeatId}
+                                //           place={place}
+                                //           rowNum={rowNum}
+                                //           type={type}
+                                //           x={x}
+                                //           y={y}
+                                //           category={seatPriceModel === undefined ? "" : seatPriceModel.name}
+                                //           price={seatPriceModel === undefined ? 0 : seatPriceModel.price}
+                                //           currency={seatPriceModel === undefined ? "" : seatPriceModel.currency}
+                                //           id={seatPriceModel === undefined ? 0 : seatPriceModel.id}
+                                //           cart={cart}
+                                //           onTicketAdd={onTicketAdd}/>
+                            );
+                        })}
+                    </Layer>
+                </Stage>
+                <div className={classes.seats}>
+                    {/*{seats.map(({*/}
+                    {/*                capacity,*/}
+                    {/*                capacityLeft,*/}
+                    {/*                eventPriceId,*/}
+                    {/*                eventSeatId,*/}
+                    {/*                place,*/}
+                    {/*                rowNum,*/}
+                    {/*                type,*/}
+                    {/*                x,*/}
+                    {/*                y*/}
+                    {/*            }, seatIndex) => {*/}
+                    {/*    const seatPriceModel = prices.find(el => el.id === eventPriceId);*/}
+
+                    {/*    return (*/}
+                    {/*        <SeatItem key={seatIndex}*/}
+                    {/*                  capacity={capacity}*/}
+                    {/*                  capacityLeft={capacityLeft}*/}
+                    {/*                  eventPriceId={eventPriceId}*/}
+                    {/*                  eventSeatId={eventSeatId}*/}
+                    {/*                  place={place}*/}
+                    {/*                  rowNum={rowNum}*/}
+                    {/*                  type={type}*/}
+                    {/*                  x={x}*/}
+                    {/*                  y={y}*/}
+                    {/*                  category={seatPriceModel === undefined ? "" : seatPriceModel.name}*/}
+                    {/*                  price={seatPriceModel === undefined ? 0 : seatPriceModel.price}*/}
+                    {/*                  currency={seatPriceModel === undefined ? "" : seatPriceModel.currency}*/}
+                    {/*                  id={seatPriceModel === undefined ? 0 : seatPriceModel.id}*/}
+                    {/*                  cart={cart}*/}
+                    {/*                  onTicketAdd={onTicketAdd}/>*/}
+                    {/*    );*/}
+                    {/*})}*/}
                 </div>
             </div>
         </div>
